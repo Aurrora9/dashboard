c20d5e1f9bb1807312be9814b35fe4369598c1ad
Aurrora9 <145787094+Aurrora9@users.noreply.github.com>
2026-08-01 15:52:24 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker for 业务分析仪表盘
+var CACHE_NAME = 'biz-dashboard-v1';
+var urlsToCache = [
+  './dashboard.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(n) { return n !== CACHE_NAME; })
+             .map(function(n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // 网络优先，离线时用缓存
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE_NAME).then(function(cache) {
+        cache.put(event.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
